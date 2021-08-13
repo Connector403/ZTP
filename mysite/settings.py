3784921333f3b09979fc3f71e869d5f07974de2c
Hafiz <hafiz-2010@hotmail.co.uk>
2021-08-13 14:11:32 +0100

    sorted out template and EX_FILE

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/home/hafiz/Desktop/Project/mysite/templates'],
+        'DIRS': ['/home/hafiz/Desktop/Project/mysite/ZTP/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,4 +128,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # excel file path
-EX_FILE = "/home/hafiz/Desktop/Project/mysite/Core/static/EnergyConsumptionDetail_updated.xlsx"
+EX_FILE = "/home/hafiz/Desktop/Project/mysite/ZTP/Core/static/EnergyConsumptionDetail_updated.xlsx"
